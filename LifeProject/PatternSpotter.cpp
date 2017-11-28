27041cd162f7916f5b8dad7b614b6214b5244c91
Kyle Neubarth <kyle@neubarth.org>
2017-11-28 12:01:51 -0700

    added more debug code

--- LifeProject/PatternSpotter.cpp
+++ LifeProject/PatternSpotter.cpp
@@ -17,16 +17,16 @@ PatternSpotter::PatternSpotter(string fileName) {
   int h = 0;
   int patternNum = 0;
   while (getline(file,temp)) {
-    //end of pattern
-    //cout << temp.length()<<";"<<h<<";";
-    
-    if (temp[0] == '/'&&temp[1] == '/') {
+      //check for pattern name ex. //patternName
+      if (temp[0] == '/'&&temp[1] == '/') {
       tempName = temp.substr(2,temp.length()-2);
       continue;
     }
     //I found the end of a pattern!
     if (temp[0] == 'k') {
       if (h>0) {
+        //This commented debug code, along with the debug code below, will print out the details of a pattern once it is read, as well as print to console all previous patterns.
+        //problem: every time a pattern is read, it scrambles the previous patterns, even though previous patterns are not accessed. wtf. The newest pattern is always correct.
         //cout << ";"<<w<<";"<<h<< ";" << patternNum <<";\n\n";
         
         patterns[patternNum].SetPattern(w,h,newPattern);
@@ -37,15 +37,15 @@ PatternSpotter::PatternSpotter(string fileName) {
         patternNum++;
         //increment total patterns
         numPatterns++;
-        //debug code
-        /*for (int i = 0;i<patternNum;i++) {
+        /*debug code
+        for (int i = 0;i<patternNum;i++) {
           patterns[i].Print();
           cout << "\n";
         }*/
       }
       w=0;
       h=0;
-      //don't try and decipher this line, it has nothing in it
+      //don't try and read this line into a pattern, it has nothing in it
       continue;
     }
     w = temp.length();
